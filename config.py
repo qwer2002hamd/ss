9d82b53237e8e64f3c1d3c21e347bc9d414eb909
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-18 13:11:31 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID", "2057416872"))
+OWNER_ID = int(getenv("OWNER_ID", "1748768168"))
 
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", "TeamSukun")
+MUST_JOIN = getenv("MUST_JOIN", "ah07v")
