4fc699ed1fbf67722f5e321fcaa4a6e151470791
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-18 15:05:06 +0300

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -12,29 +12,20 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_message(
         chat_id=msg.chat.id,
-        text=f"""Hᴇʏ {msg.from_user.mention},
-Tʜɪs ɪs {me2},
-ᴇʟʟᴏ ʙᴀʙʏ 😻,
-             🙈  ᴍᴇᴏᴡ ᴍᴇᴏᴡ ❤️
+        text=f"""✷ مـرحبا بـك عزيزي {msg.from_user.mention},
+✷ انا بـوت {me2},
+✷ لاستخراج جلسات البايروجرام و تيرمكس,
 
-𖢵 Aɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ!
-
-┏━━━━━━━━━━━━━━━━━
-┠ ๏ Uᴘᴅᴀᴛᴇꜱ :- @sukunupdates
-┠ ๏ Sᴜᴘᴘᴏʀᴛ :- @sukunsupports
-┗━━━━━━━━━━━━━━━━━
+✷ يمكنك استخراج الجلسه من الاوامر والازرار ادناه 👇🏻✔️
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(text="🙄 ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 🙄", callback_data="generate")
+                    InlineKeyboardButton(text="‹ بدء ›", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("❣️ ᴛᴇᴀᴍ sᴜᴋᴜɴ ❣️", url="https://t.me/TeamSukun"),
-                    InlineKeyboardButton("🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=OWNER_ID)
-                ],
-                [
-                    InlineKeyboardButton("ᴀᴅᴅ ѕυкυη χ мυѕι¢", url="https://t.me/sukunmusicrobot?startgroup=new")
+                    InlineKeyboardButton("‹ قـناة البوت ›", url="https://t.me/ah07v"),
+                    InlineKeyboardButton("‹ مـطور الـبوت ›", user_id=OWNER_ID)
                 ]
             ]
         ),
